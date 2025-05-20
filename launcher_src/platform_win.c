853d866224648bafcd53c7435fa875066cdd2170
Dmitri <dmitri.poterianski@gmail.com>
2025-05-20 16:17:05 -0400

    Updated Engine, Libs, Platform
    Engine:
     - Engine version 0.54 b10007
     - Update and use kryonet 2.21 => 2.22.9 from more up-to-date fork on Github crykn/kryonet
       - Switch to newer (in some case newest) version EsotericSoftware deps
       - jsonbeans 0.5 => 0.8
       - kryo 3.0.3 => 5.6.2 (Latest official!)
       - minlog 1.2 => 1.3.1
       - objenesis 1.2 => 3.2
       - reflectasm 1.07 => 1.11.9
     - Add 'Official' tab in server browser
       - For now, points to hard-coded official servers on risingear.com
       - I cannot guarantee that one or both will be running at any given time -- check back frequently!
    
    Platform:
     - Update included Windows x64 JRE 11.0.27+4 => 11.0.27+6
     - Moved Bundled/downloaded(future) JREs locations to be directly in platform suffix dir (Without its nested versioned dir; I.e. jdk-11.0.xx+x-jre)
     - Update .bat and C++ versions of Windows launchers to accommodate all of the above necessary changes
     - Removed useless ibxm.jar in all launchers' classpath argument

--- launcher_src/platform_win.c
+++ launcher_src/platform_win.c
@@ -3,12 +3,12 @@
 #include "platform.h"
 
 void laun_exec(const char* cmd) {
-	// start java-win-x86_64/jdk-14.0.2-jre/bin/java.exe
-	// start java-win-x86_64/jdk-14.0.2-jre/bin/javaw.exe
+	// start java-win-x86_64/bin/java.exe
+	// start java-win-x86_64/bin/javaw.exe
 	#if defined dedicated
-		char cmd2[32768] = "start java-win-x86_64/jdk-11.0.26+4-jre/bin/java.exe -classpath jar/risingear.min.jar;3rdparty/jar/luaj-jse-3.0.2.jar;3rdparty/jar/asm-4.0.jar;3rdparty/jar/kryo-3.0.3.jar;3rdparty/jar/kryonet-2.21.jar;3rdparty/jar/objenesis-1.2.jar;3rdparty/jar/reflectasm-1.07.jar;3rdparty/jar/jinput.jar;3rdparty/jar/lwjgl_util.jar;3rdparty/jar/lwjgl_util_applet.jar;3rdparty/jar/lwjgl.jar;3rdparty/jar/minlog-none-1.2.jar;3rdparty/jar/jsonbeans-0.5.jar;3rdparty/jar/jogg-0.0.7.jar;3rdparty/jar/jorbis-0.0.15.jar;3rdparty/jar/ibxm.jar ";
+		char cmd2[32768] = "start java-win-x86_64/bin/java.exe -classpath jar/risingear.min.jar;3rdparty/jar/luaj-jse-3.0.2.jar;3rdparty/jar/asm-4.0.jar;3rdparty/jar/kryo-5.6.2.jar;3rdparty/jar/kryonet-2.22.9.jar;3rdparty/jar/objenesis-3.2.jar;3rdparty/jar/reflectasm-1.11.9.jar;3rdparty/jar/jinput.jar;3rdparty/jar/lwjgl_util.jar;3rdparty/jar/lwjgl_util_applet.jar;3rdparty/jar/lwjgl.jar;3rdparty/jar/minlog-1.3.1.jar;3rdparty/jar/jsonbeans-0.9.jar;3rdparty/jar/jogg-0.0.7.jar;3rdparty/jar/jorbis-0.0.15.jar ";
 	#else
-		char cmd2[32768] = "start java-win-x86_64/jdk-11.0.26+4-jre/bin/javaw.exe -classpath jar/risingear.min.jar;3rdparty/jar/luaj-jse-3.0.2.jar;3rdparty/jar/asm-4.0.jar;3rdparty/jar/kryo-3.0.3.jar;3rdparty/jar/kryonet-2.21.jar;3rdparty/jar/objenesis-1.2.jar;3rdparty/jar/reflectasm-1.07.jar;3rdparty/jar/jinput.jar;3rdparty/jar/lwjgl_util.jar;3rdparty/jar/lwjgl_util_applet.jar;3rdparty/jar/lwjgl.jar;3rdparty/jar/minlog-none-1.2.jar;3rdparty/jar/jsonbeans-0.5.jar;3rdparty/jar/jogg-0.0.7.jar;3rdparty/jar/jorbis-0.0.15.jar;3rdparty/jar/ibxm.jar ";
+		char cmd2[32768] = "start java-win-x86_64/bin/javaw.exe -classpath jar/risingear.min.jar;3rdparty/jar/luaj-jse-3.0.2.jar;3rdparty/jar/asm-4.0.jar;3rdparty/jar/kryo-5.6.2.jar;3rdparty/jar/kryonet-2.22.9.jar;3rdparty/jar/objenesis-3.2.jar;3rdparty/jar/reflectasm-1.11.9.jar;3rdparty/jar/jinput.jar;3rdparty/jar/lwjgl_util.jar;3rdparty/jar/lwjgl_util_applet.jar;3rdparty/jar/lwjgl.jar;3rdparty/jar/minlog-1.3.1.jar;3rdparty/jar/jsonbeans-0.9.jar;3rdparty/jar/jogg-0.0.7.jar;3rdparty/jar/jorbis-0.0.15.jar ";
 	#endif
 	strcat(cmd2, cmd);
 	puts(cmd2);
