dimmskii/RisingearSDK

commit ed980fed812a5153a886e4dcd44a953665f43b0f

Engine Update
Engine:
 - Engine version 0.54 b10811
 - Refactored KryoClasses to non-static KryoConfig
 - Added to cvars to configure Kryo serialization
   - kryo_references : If true, each appearance of an object in the graph after the first is stored as an integer ordinal. This enables referencesto the same object and cyclic graphs to be serialized, but typically adds overhead of one byte per object. When set to trueand no reference resolver has been set, MapReferenceResolver isused.
   - kryo_compression_level : The compression level whenever DeflateSerializer is used on a network message (currently only ServerClusterMessage). Accepted values are 0-9
 - Raised server->client UDP messages per cluster from 32 to 64
 - Bypassed metatag processing (change get to getraw) for persistance table logic in EntityData

version
@@ -1,1 +1,1 @@
-005410752
+005410811

data/sdkbase/default.cfg
@@ -66,6 +66,10 @@ seta name	"Player"
 // License Agreement
 seta eula_accepted "0"
 
+// Kryo Config
+seta kryo_references			"1"
+seta kryo_compression_level		"4"
+
 // Movement Binds
 bind a "+moveleft"
 bind d "+moveright"

commit 84db008c4697506c1ad8e2b33c66123ae0f3afe0

Engine and SDK Base Update
Engine:
 - Engine version 0.54 b10923
 - Added Table:getHeaderRow() and Table:getRow(int rowIndex)
 - Changed LuaTableModel:getColumnName to LuaTableModel:getColumnHeader
 - Fixed column headers in TableContainers like the one in editor not scrolling horizontally together with rows
 - Fixed regression introduced in commit ed980fe: Local server discovery broken after making KryoClasses dynamic
 - Server browser has column headers visible because now uses the (currently buggy) TableContainer
 - Server browser now reports responsive remote servers' versions as well
 - Control options input table added four commands: Look Up/Down/Left/Right

SDK Base:
 - Editor: Updated Ent Explorer table model function name to reflect engine changes

data/sdkbase/lua/gamemodes/editor/gui/gui_window_explorer.lua
@@ -83,7 +83,7 @@ if (CLIENT) then
 			local table_container = fgui.createTableContainer(cont)
 			explorer_list = table_container:getTable()
 			explorer_list:setModel(fgui.newTableModel({
-				getColumnName = function(iCol)
+				getColumnHeader = function(iCol, labelAppearance)
 					if iCol == 0 then
 						return "ID" -- TODO Stringadactyl
 					elseif iCol == 1 then

version
@@ -1,1 +1,1 @@
-005410862
+005410923